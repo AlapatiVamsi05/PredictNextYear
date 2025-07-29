092fc1955497b73b237832d37f7aafb5adee6b3f
AlapatiVamsi05 <alapativamsi05@gmail.com>
2025-07-29 09:51:59 +0530

    nice year update 1

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.getElementById("predictYear").addEventListener("click", function() {
     let year = parseInt(document.getElementById("yearInput").value);
-    let result = 1+year;
-    console.log("The next year is: " + result);
     let popup = document.getElementById("popup");
     let resultElement = document.getElementById("result");
-    resultElement.innerText = "The next year is: " + result;
+    if(isNaN(year)){
+        resultElement.innerText = "Please enter a year.";
+    }
+    else if(year < 0) {
+        let resultyear = year+1;
+        resultElement.innerText = "The next year is: " + resultyear +" B.C";
+    }else if (year+1!=69){
+        let resultyear = year+1;
+        resultElement.innerText = "The next year is: " + resultyear;
+    }else{
+        let resultyear = year+1;
+        resultElement.innerText = "The next year is: " + resultyear +" he he he he, nice year";
+    }
     popup.style.display = "flex";
 });
